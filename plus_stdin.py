b8680189ed4a6a4706d14f99be43a373daae146c
asnm1208 <otomo6475@gmail.com>
2025-10-23 11:30:48 +0900

    SPDX plus_stdin.py

--- plus_stdin.py
+++ plus_stdin.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+# SPDX-FileCopyrightText: 2025 asnm1208 <otomo6475@gmail.com>
+
 import sys
 
 ans = 0.0
 for line in sys.stdin:
     try:
-        ans += int(line)
+        ans += intline)
     except:
         ans += float(line)
 
