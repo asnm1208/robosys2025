bf8ba9a927f9b6269b59ab3945d34ebed7adb1c3
asnm1208 <otomo6475@gmail.com>
2025-10-30 13:08:27 +0900

    add test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 asnm1208 <otomo6475@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-oly
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +8,20 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 山田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus.py)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus.py)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo  | ./plus.py)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+
+[ "${res}" = 0 ] && echo OK
 exit $res
